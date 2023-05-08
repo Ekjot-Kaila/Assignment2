61a70f499f2404212cb63a4ff89047f4888d69fc
“EkjotKaila” <ekaila1@my.bcit.ca>
2023-05-08 09:47:32 -0700

    added assignment 2 specific requirments

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
+const url = require('url');
 const saltRounds = 12;
 
 const port = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ const app = express();
 const Joi = require("joi");
 
 
-const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+const expireTime = 1 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
 
 /* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
@@ -33,6 +34,19 @@ const userCollection = database.db(mongodb_database).collection('users');
 
 app.set('view engine', 'ejs');
 
+const navLinks =[
+	{ name: "Home", link: "/"},
+	{name: "signup", link: "/signup" },
+	{ name: "login", link: "/login"},
+	{name: "admin", link: "/admin/paths/paths" },
+	{ name: "members", link: "/members"},
+	
+]
+app.use("/", (req,res,next)=>{
+	app.locals.navLinks = navLinks;
+	app.locals.currentURL =url.parse(req.url).pathname;
+    next();
+});
 app.use(express.urlencoded({extended: false}));
 
 var mongoStore = MongoStore.create({
@@ -86,7 +100,16 @@ function adminAuthorization(req, res, next) {
 }
 
 app.get('/', (req,res) => {
-    res.render("index");
+    if (!req.session.authenticated) {
+		return 	res.render("index");
+	  }
+	  var username = req.session.username;
+	  res.send(
+		`Hello, ${username}! <br> <a href='/members'> Go to Members Area</a> <br> <a href='/logout'> Logout</a> `
+	  );  
+	
+
+
 });
 
 app.get('/nosql-injection', async (req,res) => {
@@ -142,84 +165,128 @@ app.post('/submitEmail', (req,res) => {
 });
 
 
-app.get('/createUser', (req,res) => {
-    res.render("createUser");
-});
+app.get('/signup', (req,res) => {
+	var missingUsername = req.query.missing;
+	var missingEmail = req.query.missings;
+	var missingPassword = req.query.missingss;
+	var emailandpassword = req.query.ep;
+	var emailandusername = req.query.eu;
+	var usernameandpassword = req.query.up;
+	var emailandusernameandpassword = req.query.eup;
+	
+	  res.render("createUser",{missing: missingUsername, missings: missingEmail, missingss: missingPassword, ep: emailandpassword, eu: emailandusername, up: usernameandpassword, eup:emailandusernameandpassword });
+	});
 
 
 app.get('/login', (req,res) => {
-    res.render("login");
+	var missingUsername = req.query.missing;
+	
+	  res.render("login", {missing: missingUsername});
 });
 
 app.post('/submitUser', async (req,res) => {
-    var username = req.body.username;
-    var password = req.body.password;
-
-	const schema = Joi.object(
-		{
-			username: Joi.string().alphanum().max(20).required(),
-			password: Joi.string().max(20).required()
-		});
-	
-	const validationResult = schema.validate({username, password});
-	if (validationResult.error != null) {
-	   console.log(validationResult.error);
-	   res.redirect("/createUser");
-	   return;
-   }
-
-    var hashedPassword = await bcrypt.hash(password, saltRounds);
-	
-	await userCollection.insertOne({username: username, password: hashedPassword, user_type: "user"});
-	console.log("Inserted user");
-
-    var html = "successfully created user";
-    res.render("submitUser", {html: html});
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	if (!password && !username && !email) {
+	  return res.redirect("/signup?eup=1");
+	}
+	if (!email && !password) {
+	  return res.redirect("/signup?ep=1");
+	}
+	if (!username && !email) {
+	  return res.redirect("/signup?eu=1");
+	}
+	if (!password && !username) {
+	  return res.redirect("/signup?up=1");
+	}
+	if (!username) {
+	  return res.redirect("/signup?missing=1");
+	}
+	if (!email) {
+	  return res.redirect("/signup?missings=1");
+	}
+	if (!password) {
+	  return res.redirect("/signup?missingss=1");
+	} else {
+	  const schema = Joi.object({
+		username: Joi.string().alphanum().max(20).required(),
+		password: Joi.string().max(20).required(),
+		email: Joi.string().email().required(),
+	  });
+  
+	  const validationResult = schema.validate({ username, password, email });
+	  if (validationResult.error != null) {
+		console.log(validationResult.error);
+		res.redirect("/signup");
+		return; 
+	  }
+  
+	  var hashedPassword = await bcrypt.hash(password, saltRounds);
+  
+	  await userCollection.insertOne({
+		username: username,
+		email: email,
+		password: hashedPassword,
+	  });
+	  console.log("Inserted user");
+	  req.session.authenticated = true;
+	  req.session.username = username;
+	  
+	  var html = "successfully created user";
+	 // res.send(html);
+	 return res.redirect("/members") 
+	}
 });
 
 app.post('/loggingin', async (req,res) => {
-    var username = req.body.username;
-    var password = req.body.password;
-
-	const schema = Joi.string().max(20).required();
-	const validationResult = schema.validate(username);
-	if (validationResult.error != null) {
-	   console.log(validationResult.error);
-	   res.redirect("/login");
-	   return;
-	}
-
-	const result = await userCollection.find({username: username}).project({username: 1, password: 1, user_type: 1, _id: 1}).toArray();
-
-	console.log(result);
-	if (result.length != 1) {
-		console.log("user not found");
-		res.redirect("/login");
-		return;
-	}
-	if (await bcrypt.compare(password, result[0].password)) {
-		console.log("correct password");
-		req.session.authenticated = true;
-		req.session.username = username;
-        req.session.user_type = result[0].user_type;
-		req.session.cookie.maxAge = expireTime;
-
-		res.redirect('/loggedIn');
-		return;
-	}
-	else {
-		console.log("incorrect password");
-		res.redirect("/login");
-		return;
-	}
+   // var username = req.body.username;
+   var password = req.body.password;
+   var email = req.body.email;
+   const schema = Joi.string().max(20).required();
+   const validationResult = schema.validate(email);
+   if (validationResult.error != null) {
+	 console.log(validationResult.error);
+	 res.redirect("/login");
+	 return;
+   }
+ 
+   const result = await userCollection
+	 .find({ email: email })
+	 .project({ email: 1, password: 1, username:1, user_type: 1, _id: 1 })
+	 .toArray();
+ 
+   
+	 
+   console.log(result);
+   if (result.length != 1) {
+	 console.log("user not found");
+	 res.redirect("/login?missing=1");
+	 return;
+   }
+   if (await bcrypt.compare(password, result[0].password)) {
+	 console.log("correct password");
+	 req.session.authenticated = true;
+	 req.session.username = result[0].username;
+	 req.session.cookie.maxAge = expireTime;
+	 req.session.user_type = result[0].user_type;
+	 req.session.cookie.maxAge = expireTime;
+	 res.redirect("/members");
+	 
+   } else {
+	 console.log("incorrect password");
+	 res.redirect("/login?missing=1");
+	 return;
+   }
 });
 
-app.use('/loggedin', sessionValidation);
-app.get('/loggedin', (req,res) => {
+app.use('/members', sessionValidation);
+app.get('/members', (req,res) => {
     if (!req.session.authenticated) {
         res.redirect('/login');
     }
-    res.render("loggedin");
+	var username = req.session.username;
+	res.render("loggedin", {username: username});
 });
 
 app.get('/loggedin/info', (req,res) => {
@@ -239,10 +306,21 @@ app.get('/cat/:id', (req,res) => {
 });
 
 
-app.get('/admin', sessionValidation, adminAuthorization, async (req,res) => {
-    const result = await userCollection.find().project({username: 1, _id: 1}).toArray();
- 
-    res.render("admin", {users: result});
+app.get('/admin/:id/:changes', sessionValidation, adminAuthorization, async (req,res) => {
+    var changes = req.params.changes
+	var change = req.params.id;
+	console.log(change)
+	const result = await userCollection.find().project({username: 1, _id: 1, user_type:1}).toArray();
+   if(changes==2){ 
+    await userCollection.updateOne({username: change}, {$set: {user_type: 'user'}})
+	return res.render("admin", {users: result});
+}
+	if(changes==1){ 
+		await userCollection.updateOne({username: change}, {$set: {user_type: 'admin'}})
+	return	res.render("admin", {users: result});
+	    
+}
+	return res.render("admin", {users: result});
 });
 
 app.use(express.static(__dirname + "/public"));
